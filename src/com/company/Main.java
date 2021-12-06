6d4c4d06c69d723e7180aa25d697d8fe31dc02d6
muratbaiguchukov <ambkg-muratb@yandex.ru>
2021-12-06 13:20:25 +0600

    add homeworks 13.2 Z.A

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,20 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.print("Привет, GitHub ");
+        Student roman = new Student();
+        System.out.println("Student Roman year of enrollment: " + roman.year_of_enrollment + "studying for " +  roman.course +  "he is " + roman.age + "years old");
+        Student vlad = new Student();
+        vlad.course = 3;
+        vlad.year_of_enrollment = 2019;
+        vlad.age = 20;
+        System.out.println("Student Vlad year of enrollment: " + vlad.year_of_enrollment + " studying for " +  vlad.course +  " he is " + vlad.age + " years old ");
+        Student igor = new Student();
+        igor.course = 5;
+        igor.year_of_enrollment = 2017;
+        igor.age = 22;
+        System.out.println("Student Igor year of enrollment: " + igor.year_of_enrollment + " studying for " +  igor.course +  " he is " + igor.age + " years old ");
+
+
+
     }
 }
---
+++ src/com/company/Student.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Student {
+    public int course = 1;
+    public int age = 18;
+    public int year_of_enrollment = 2021;
+
+}
