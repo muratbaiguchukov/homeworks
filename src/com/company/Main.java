70bfe4c4249b74a0d7c55869daf2330a5ad0eeb2
muratbaiguchukov <ambkg-muratb@yandex.ru>
2021-12-14 22:32:40 +0600

    add homeworks 20

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,17 +1,28 @@
 package com.company;
 
 import com.company.task3.Credit;
+import com.company.task4.Circle;
 
 public class Main {
 
     public static void main(String[] args) {
-        Credit credit = new Credit();
-        Credit cre1 = new Credit("Иванов", 10000, "one year", 15);
-        cre1.Id = "Иванов";
-        cre1.amount = 10000;
-        cre1.period = "one year";
-        cre1.percentage = 15;
+        //Credit credit = new Credit();
+        //Credit cre1 = new Credit("Иванов", 10000, "one year", 15);
+        //cre1.Id = "Иванов";
+       // cre1.amount = 10000;
+        //cre1.period = "one year";
+        //cre1.percentage = 15;
 
-        System.out.println("Ура");
+        //Circle ci = new Circle();
+        //ci.Radius(6.5);
+        //PI(3.14);
+        //ci.mult(double PI, double Radius);
+        //System.out.println(mult);
+
+        double s = Circle.sum(3.14, 6.5);
+        System.out.println(s);
+
+
+
     }
 }
